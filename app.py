d0ec04404646d55df203b05c032e2d01f83e72c7
umaraniselvaraj <umarani.selvaraj@gmail.com>
2023-01-28 17:13:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+"""Main application file"""
+from flask import Flask
+app=Flask(_name_)
+
+@app.route('/<random_string>')
+def returnBackwardsString(random_string):
+    """Reverse and return the provided URI"""
+    return "Breaking the unit test.."
+if _name_=='_main_':
+    app.run(host='0.0.0.0',port=8080)
