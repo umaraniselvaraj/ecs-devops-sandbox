42bdd5e22b9ac75c9846e636f54dd71533dc7963
umaraniselvaraj <umarani.selvaraj@gmail.com>
2023-01-29 15:30:05 +0530

    app file changed

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 """Main application file"""
 from flask import Flask
-app=Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    return "Breaking the unit test..."
-if _name_=='_main_':
-    app.run(host='0.0.0.0',port=8080)
+    return "".join(reversed(random_string))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
